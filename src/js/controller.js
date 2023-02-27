a7dc5340dd34da99cd7bdb7ace0a056642dc3f8d
ard-rgb <ardrgb@gmail.com>
2023-02-27 15:36:57 +0530

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -139,10 +139,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -151,7 +147,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
@@ -160,5 +155,4 @@ const clearBookmarks = function () {
   localStorage.clear('bookmarks');
 };
 
-console.log('welcome!');
 // clearBookmarks();
